554df604f12fd8d01efd1b34ed04f2bff94b552e
Nrdila <164182313+Nrdila@users.noreply.github.com>
2024-03-20 22:38:06 +0800

    Update Container

--- flutter_widgets/lib/homepage.dart
+++ flutter_widgets/lib/homepage.dart
@@ -20,3 +20,20 @@ class HomePage extends StatelessWidget {
               'Emang kamu gak capek cantik terus?',
               style: TextStyle(fontSize: 16.0),
             ),
+            ////////////////////////////////////////////////////////////////////////////////////////////
+            const SizedBox(height: 20.0),
+            const Text(
+              '2. Container:',
+              style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
+            ),
+            Container(
+              width: 100.0,
+              height: 100.0,
+              color: Color.fromARGB(255, 1, 215, 223),
+              child: const Center(
+                child: Text(
+                  'Hai Manis',
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+            ),
